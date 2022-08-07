c1a2e8430697c510780dde8a163ab433e7d724be
Viktor Holovin <holovin@mail.com>
2022-08-07 10:05:24 +0200

    Start project

---
+++ src/com/holovin/reversejava/Reverse.java
@@ -0,0 +1,23 @@
+package com.holovin.reversejava;
+
+public class Reverse {
+	
+	public static void main(String[] args) {
+		System.out.println(reverse("Hello, world!"));
+	}
+	
+	public static String reverse(String input) {		
+
+		String inputWords[] = input.split(" ");
+		String outputWords[] = new String[inputWords.length];
+		int i = 0;
+		for (String inputWord : inputWords) {
+			String reverse = new 				StringBuilder(inputWord).reverse().toString();
+			outputWords[i] = reverse;
+			i++;
+		}
+
+		return String.join(" ", outputWords);
+	}
+
+}
---
+++ test/com/holovin/reversejava/ReverseTest.java
@@ -0,0 +1,13 @@
+package com.holovin.reversejava;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
+
